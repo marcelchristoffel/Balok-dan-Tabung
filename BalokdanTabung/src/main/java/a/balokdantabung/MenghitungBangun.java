446ed2092a231cd1f641c888ed22646d72824663
marcelchristoffel <106792002+marcelchristoffel@users.noreply.github.com>
2022-06-03 11:05:04 +0700

    Add files via upload

---
+++ BalokdanTabung/src/main/java/a/balokdantabung/MenghitungBangun.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a.balokdantabung;
+
+/**
+ *
+ * @author lenovo
+ */
+public interface MenghitungBangun {
+    double PHI = 3.14;
+    double hitungLuas();
+    double hitungKeliling();
+}
---
+++ BalokdanTabung/src/main/java/a/balokdantabung/MenghitungRuang.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a.balokdantabung;
+
+/**
+ *
+ * @author lenovo
+ */
+public interface MenghitungRuang {
+    double hitungVolume();
+    double hitungLuasP();
+}
